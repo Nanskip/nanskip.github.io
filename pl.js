718e5ffe63350a5aa25af258b5d9724f08ce534f
Nazar Polyvoda <nanskip.oof@gmail.com>
2025-07-17 04:01:18 +0200

    test

---
+++ pl.js
@@ -0,0 +1,7 @@
+(function() {
+  var s = document.createElement("script");
+  s.defer = true;
+  s.dataset.domain = "nanskip.dev";
+  s.src = "https://plausible.io/js/script.js";
+  document.head.appendChild(s);
+})();
